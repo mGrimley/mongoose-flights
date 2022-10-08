4a5b786c93c375bb696d565095b696b2f02654a1
mGrimley <mich@elgrimley.com>
2022-10-07 17:02:39 -0700

    Adds new flight page

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,6 +2,7 @@ const Flight = require('../models/flight')
 
 module.exports = {
     index,
+    new: newMovie,
 }
 
 function index(req, res) {
@@ -11,4 +12,8 @@ function index(req, res) {
     Flight.find({}, function(err, flights) {
         res.render('flights/index', { title: 'All Flights', flights });
     });
+}
+
+function newMovie(req, res) {
+    res.render('flights/new', {title: 'Add Flight'})
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const flightCtrl = require('../controllers/flights')
 
 router.get('/', flightCtrl.index)
+router.get('/new', flightCtrl.new)
 
 module.exports = router
